25bb75d624cdc89bd08f753672a1099641f027e2
vugar0909a <132210763+vugar0909a@users.noreply.github.com>
2023-05-01 01:09:51 +0400

    Update __init__.py

--- strings/__init__.py
+++ strings/__init__.py
@@ -39,12 +39,12 @@ for filename in os.listdir(r"./strings"):
 for filename in os.listdir(r"./strings/langs/"):
     if "en" not in languages:
         languages["en"] = yaml.safe_load(
-            open(r"./strings/langs/az.yml", encoding="utf8")
+            open(r"./strings/langs/en.yml", encoding="utf8")
         )
         languages_present["en"] = languages["en"]["name"]
     if filename.endswith(".yml"):
         language_name = filename[:-4]
-        if language_name == "az":
+        if language_name == "en":
             continue
         languages[language_name] = yaml.safe_load(
             open(r"./strings/langs/" + filename, encoding="utf8")
